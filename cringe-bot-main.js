6188e496e8121bc47849aa071ad231bad4db49b8
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-09-24 23:43:30 +0300

    Added getcringe and help commands

--- cringe-bot-main.js
+++ cringe-bot-main.js
@@ -15,15 +15,27 @@ const connect = (async () => await client.connect())().then(
 
 const COMMAND_TIMEOUT = 30000;
 
+bot.help((ctx) => {
+  ctx.reply(
+    '/cringe – крінжанути з когось\n' +
+      '/baza – закинути базу\n' +
+      '/mycringe – дізнатися свій рівень крінжа чи бази\n' +
+      '/getcringe + <username> – дізнатися рівень крінжа чи бази когось іншого\n' +
+      '/topcringe – топ людей по крінжі в чаті\n' +
+      '/topbaza – топ людей по базі в чаті\n' +
+      '/donate – будь ласка задонать)\n'
+  );
+});
+
 bot.command('cringe', async (ctx) => {
   try {
     const reply = ctx.update.message.reply_to_message;
     if (reply) {
-
       const users = client.db().collection('users');
 
       const username = reply.from.username || reply.sender_chat.title;
-      const isChannel = !!reply.sender_chat && reply.sender_chat.type === 'channel';
+      const isChannel =
+        !!reply.sender_chat && reply.sender_chat.type === 'channel';
 
       const user = await users.findOne({
         username: username,
@@ -44,7 +56,6 @@ bot.command('cringe', async (ctx) => {
         ctx.reply(`${emoji} крінж`);
         console.log(username, '+1');
       } else {
-
         if (!user.lastUsedBy) {
           await users.updateOne(
             { username: username, chatId: ctx.update.message.chat.id },
@@ -75,7 +86,6 @@ bot.command('cringe', async (ctx) => {
             { reply_to_message_id: ctx.message.message_id }
           );
         }
-
       }
     } else {
       ctx.reply(
@@ -98,11 +108,11 @@ bot.command('baza', async (ctx) => {
   try {
     const reply = ctx.update.message.reply_to_message;
     if (reply) {
-
       const users = client.db().collection('users');
 
       const username = reply.from.username || reply.sender_chat.title;
-      const isChannel = !!reply.sender_chat && reply.sender_chat.type === 'channel';
+      const isChannel =
+        !!reply.sender_chat && reply.sender_chat.type === 'channel';
 
       const user = await users.findOne({
         username: username,
@@ -122,7 +132,6 @@ bot.command('baza', async (ctx) => {
         ctx.reply(`${emoji} база`);
         console.log(username, '-1');
       } else {
-
         if (!user.lastUsedBy) {
           await users.updateOne(
             { username: username, chatId: ctx.update.message.chat.id },
@@ -153,13 +162,64 @@ bot.command('baza', async (ctx) => {
             { reply_to_message_id: ctx.message.message_id }
           );
         }
-
       }
     } else {
       ctx.reply(
         'Команди cringe та baza потрібно використовувати у відповідь на повідомлення',
         { reply_to_message_id: ctx.message.message_id }
       );
+    }
+  } catch (err) {
+    console.log(err);
+    ctx.replyWithAnimation(
+      'CgACAgQAAxkBAAIGdWMGRgQzE71bCkapOT7XhUl0-IA3AAKjAgACRZT0UWWh8_f0jFRtKQQ',
+      { caption: 'Помилка!' },
+      { reply_to_message_id: ctx.message.message_id }
+    );
+    await fs.promises.appendFile('log.txt', err.stack + '\n\n\n');
+  }
+});
+
+bot.command('getcringe', async (ctx) => {
+  try {
+    const users = client.db().collection('users');
+    const neededUsername = ctx.message.text
+      .split(' ')
+      .slice(1)
+      .join(' ')
+      .replace('@', '');
+
+    if (neededUsername) {
+
+      const user = await users.findOne({
+        username: neededUsername,
+        chatId: ctx.update.message.chat.id,
+      });
+
+      if (!user) {
+        ctx.reply(
+          `З <a href="t.me/${user.username}">${user.username}</a> ще не крінжували або його немає в цьому чаті!`,
+          { reply_to_message_id: ctx.message.message_id, parse_mode: 'HTML', disable_web_page_preview: true }
+        );
+      } else {
+        if (user.cringeRate < 0) {
+          const bazaEmoji = String.fromCodePoint(0x1f7e2);
+          ctx.reply(
+            `${bazaEmoji} Рівень бази <a href="t.me/${user.username}">${user.username}</a> в цьому чаті: ${-1*user.cringeRate}`,
+            { reply_to_message_id: ctx.message.message_id, parse_mode: 'HTML', disable_web_page_preview: true }
+          );
+        } else {
+          const cringeEmoji = String.fromCodePoint(0x1f534);
+          ctx.reply(
+            `${cringeEmoji} Рівень крінжа <a href="t.me/${user.username}">${user.username}</a> в цьому чаті: ${user.cringeRate}`,
+            { reply_to_message_id: ctx.message.message_id, parse_mode: 'HTML', disable_web_page_preview: true }
+          );
+        }
+      }
+    } else {
+      ctx.reply('Вкажіть юзернейм людини, крінж якої хочете дізнатись', {
+        reply_to_message_id: ctx.message.message_id,
+      });
     }
   } catch (err) {
     console.log(err);
@@ -185,7 +245,6 @@ bot.command('mycringe', async (ctx) => {
         reply_to_message_id: ctx.message.message_id,
       });
     } else {
-
       if (me.cringeRate < 0) {
         const bazaEmoji = String.fromCodePoint(0x1f7e2);
         ctx.reply(
@@ -199,7 +258,6 @@ bot.command('mycringe', async (ctx) => {
           { reply_to_message_id: ctx.message.message_id }
         );
       }
-
     }
   } catch (err) {
     console.log(err);
@@ -224,14 +282,12 @@ bot.command('topcringe', async (ctx) => {
     let place = 1;
     await topCursor.forEach((user) => {
       if (user.cringeRate >= 0) {
-
         if (user.isChannel)
           reply.push(`${place}. ${user.username}: ${user.cringeRate} `);
         else
           reply.push(
             `${place}. <a href="t.me/${user.username}">${user.username}</a>: ${user.cringeRate} `
           );
-
       }
       place++;
     });
@@ -262,7 +318,6 @@ bot.command('topbaza', async (ctx) => {
     let place = 1;
     await topCursor.forEach((user) => {
       if (user.cringeRate <= 0) {
-
         if (user.isChannel)
           reply.push(`${place}. ${user.username}: ${-1 * user.cringeRate}`);
         else
@@ -271,14 +326,16 @@ bot.command('topbaza', async (ctx) => {
               -1 * user.cringeRate
             } `
           );
-
       }
       place++;
     });
-    ctx.reply(`Топ-${reply.length} по базі в цьому чаті:\n` + reply.join('\n'), {
-      parse_mode: 'HTML',
-      disable_web_page_preview: true,
-    });
+    ctx.reply(
+      `Топ-${reply.length} по базі в цьому чаті:\n` + reply.join('\n'),
+      {
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+      }
+    );
   } catch (err) {
     console.log(err);
     ctx.replyWithAnimation(
@@ -290,11 +347,11 @@ bot.command('topbaza', async (ctx) => {
   }
 });
 
-bot.command('donate', ctx => {
+bot.command('donate', (ctx) => {
   ctx.reply(
     `Можна підтримати автора, якщо скинути <a href="https://send.monobank.ua/jar/21zMu1w55M">йому</a> пару гривень`,
     { parse_mode: 'HTML', disable_web_page_preview: true }
   );
-})
+});
 
 bot.launch().then(console.log('Bot is running'));
